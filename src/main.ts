dd19f99478156e235e55ad400c97a63d8974243b
Mortimer <mortyh@gmail.com>
2018-05-16 00:32:38 +0200

    Move axios setup outside Vue app lifecycle

--- src/main.ts
+++ src/main.ts
@@ -6,28 +6,25 @@ import { RouteViews } from './models';
 import router from './router';
 import store from './store';
 
-
+axios.defaults.baseURL = 'http://localhost:5000/api/';
+if (localStorage.getItem('token')) {
+    axios.defaults.headers['Authorization'] = 'Bearer ' + localStorage.getItem('token');
+}
+axios.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response.status === 401) {
+            store.dispatch('AUTH_LOGOUT');
+            router.push('/login');
+        }
+        return Promise.reject(error);
+    }
+);
 
 const app = new Vue({
     el: '#app',
     template: '<router-view></router-view>',
     router,
-    store,
-    created() {
-        axios.defaults.baseURL = 'http://localhost:5000/api/';
-        if (localStorage.getItem('token')) {
-            axios.defaults.headers['Authorization'] = 'Bearer ' + localStorage.getItem('token');
-        }
-        axios.interceptors.response.use(
-            response => response,
-            error => {
-                if (error.response.status === 401) {
-                    this.$store.dispatch('AUTH_LOGOUT');
-                    this.$router.push('/login');
-                }
-                return Promise.reject(error);
-            }
-        );
-    }
+    store
     // mixins: [VueRouter],
 });
