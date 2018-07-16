f16ba04bb4b0b1b6bee3045007edf01a6b6084e8
Mortimer <mortyh@gmail.com>
2018-07-17 01:57:50 +0200

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,5 @@ cp index.html $TRAVIS_BUILD_DIR/tmp/
 cp -R css $TRAVIS_BUILD_DIR/tmp/
 cp -R dist $TRAVIS_BUILD_DIR/tmp/
 
-rsync --quiet -r --delete $TRAVIS_BUILD_DIR/tmp/ travis@rancor.mortimer.nu:/var/travis/runit-web/$TRAVIS_BUILD_ID
-ssh -t travis@rancor.mortimer.nu 
-    "rm /var/travis/runit-web/current && ln -s /var/travis/runit-web/$TRAVIS_BUILD_ID /var/travis/runit-web/current"
+rsync -r --delete $TRAVIS_BUILD_DIR/tmp/ travis@rancor.mortimer.nu:/var/travis/runit-web/$TRAVIS_BUILD_ID
+ssh -t travis@rancor.mortimer.nu "rm /var/travis/runit-web/current && ln -s /var/travis/runit-web/$TRAVIS_BUILD_ID /var/travis/runit-web/current"
