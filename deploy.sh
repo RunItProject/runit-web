1aa21fe436d95951c71cfe42bb9d8ccd32d98ffd
Mortimer <mortyh@gmail.com>
2018-09-20 17:30:40 +0200

    Include /static/ in deploy folder.

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 mkdir $TRAVIS_BUILD_DIR/tmp
 cp index.html $TRAVIS_BUILD_DIR/tmp/
-cp -R css $TRAVIS_BUILD_DIR/tmp/
 cp -R dist $TRAVIS_BUILD_DIR/tmp/
+cp -R static $TRAVIS_BUILD_DIR/tmp/
 
 rsync -r --delete $TRAVIS_BUILD_DIR/tmp/ travis@rancor.mortimer.nu:/var/travis/runit-web/$TRAVIS_BUILD_ID
 ssh -t travis@rancor.mortimer.nu "rm /var/travis/runit-web/current && ln -s /var/travis/runit-web/$TRAVIS_BUILD_ID /var/travis/runit-web/current"
